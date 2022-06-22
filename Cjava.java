5e8c32748aca06b61616fbc1f8d79eced82cfa07
sankhajit1998 <93027838+sankhajit1998@users.noreply.github.com>
2022-06-22 13:40:59 +0530

    Add files via upload

---
+++ Cjava.java
@@ -0,0 +1,8 @@
+class Cjava {
+	public static void main(String[] args) {
+		System.out.println("Name: Sankhajit Roy.");
+		System.out.println("Course: Java Web programming.");
+		System.out.println("Stream: Computer Science Engineering");
+	}
+	
+}
---
+++ Vjava.java
@@ -0,0 +1,17 @@
+public class Vjava {
+	public static void main (String[] args){
+		int num1 = 45, num2 = 5;
+		System.out.print("The addition of two numbers is ");
+		System.out.println(num1+num2);
+		
+		System.out.print("The substraction of two numbers is ");
+		System.out.println(num1-num2);
+		
+		System.out.print("The multiplication of two numbers is ");
+		System.out.println(num1*num2);
+		
+		System.out.print("The division of two numbers is ");
+		System.out.println(num1/num2);
+	}
+
+}
